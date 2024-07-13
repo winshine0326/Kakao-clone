d5df13e3b37faf4ce73e623ff343c95748db010a
winshine0326 <162104541+winshine0326@users.noreply.github.com>
2024-07-14 02:07:24 +0900

    style: maintxt 애니메이션 효과 추가

--- files/script.js
+++ files/script.js
@@ -21,3 +21,13 @@ headsubul.addEventListener('mouseout',()=>{
     headsubdiv.style.maxHeight='0px';
     headsubul.style.visibility='hidden';
 })
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector('.maintext');
+    text.style.animation = 'slideUp 1.1s 0.2s ease-out forwards';
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector('.mainp');
+    text.style.animation = 'slideUp 1s 0.6s ease-out forwards';
+});
