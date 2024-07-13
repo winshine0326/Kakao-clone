2336e8f877422a0f7096c6b0a2c51352fb2ce6a8
winshine0326 <162104541+winshine0326@users.noreply.github.com>
2024-07-14 01:13:18 +0900

    feat: header sub div 구현

---
+++ files/script.js
@@ -0,0 +1,22 @@
+var survice = document.getElementById("survice");
+var headsubdiv = document.getElementById("headsubdiv");
+var headsubul = document.getElementById("headsubul");
+
+survice.addEventListener('mouseover',()=>{
+  headsubdiv.style.maxHeight='85px';
+  headsubul.style.visibility='visible'
+})
+survice.addEventListener('mouseout',()=>{
+    headsubdiv.style.maxHeight='0px';
+    headsubul.style.visibility='hidden';
+})
+
+headsubul.addEventListener('mouseover',()=>{
+    headsubdiv.style.maxHeight='85px';
+    headsubul.style.visibility='visible'
+  })
+headsubul.addEventListener('mouseout',()=>{
+    headsubdiv.style.maxHeight='0px';
+    headsubul.style.visibility='hidden';
+})
+  
